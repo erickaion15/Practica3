4c4859f1010cda98950379998e2e8861941705e0
Erick Diaz Zamora <98294959+erickaion15@users.noreply.github.com>
2022-11-30 23:19:30 -0600

    Update TrendUpdate.py

--- TrendUpdate.py
+++ TrendUpdate.py
@@ -8,16 +8,11 @@ carga_RED = 0
 oid_cpu1 = '1.3.6.1.2.1.25.3.3.1.2.196608'
 oid_cpu2 = '1.3.6.1.2.1.25.3.3.1.2.196609'
 
-oid_ram ="1.3.6.1.4.1.2021.4.6.0"#antes del 0 un 5 para ram total
+oid_ram ="1.3.6.1.4.1.2021.4.6.0"
 oid_ram2 ="1.3.6.1.4.1.2021.4.5.0"
-#entrada de trafico de red
 oid_red= "1.3.6.1.2.1.2.2.1.16.2"
-oid_red1 =".1"
-oid_red2 =".2"
-#salida de trafico de red
 oid_red_s= "1.3.6.1.2.1.2.2.1.10.2"
-oid_red1_s =".1"
-oid_red2_s =".2"
+
 
 def actualizar_todo(comunidad,localhost):
     while 1:
@@ -44,4 +39,3 @@ def actualizar_todo(comunidad,localhost):
         print (rrdtool.error())
         time.sleep(300)
 
-#actualizar_todo(comunidad="comunidadSNMP",localhost="Localhost")
